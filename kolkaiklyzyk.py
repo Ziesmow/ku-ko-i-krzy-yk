a1b9d49eac81fb74c6840743faf85025f89b3e74
Ziesmow <Ziesmow@proton.me>
2024-12-26 11:59:52 +0100

    wyświetlanie +/- działa

---
+++ kolkaiklyzyk.py
@@ -0,0 +1,42 @@
+
+a1 = 0
+a2 = 0
+a3 = 0
+b1 = 0
+b2 = 0
+b3 = 1
+c1 = 0
+c2 = 0
+c3 = 0
+
+kolkaiklyk = [
+    a1 ,
+    a2 ,
+    a3 ,
+    b1 ,
+    b2 ,
+    b3 ,
+    c1 ,
+    c2 ,
+    c3 ,
+]
+
+
+
+
+def printkolka ():
+    for abc in kolkaiklyk:
+        if abc == 0 :
+            print("     |")
+        elif abc == 1 :
+            print("  / |")
+
+
+
+printkolka()
+
+
+
+
+
+
