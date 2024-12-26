2ccee96b25be52bba9732aa8727d4ddca4684e5e
Ziesmow <Ziesmow@proton.me>
2024-12-26 13:22:13 +0100

    dodano stawianie x i main pentle

--- kolkaiklyzyk.py
+++ kolkaiklyzyk.py
@@ -1,18 +1,15 @@
 
 kolka = {
-    'a1' : 1,
+    'a1' : 0,
     'a2' : 0,
-    'a3' : 2,
-    'b1' : 2,
-    'b2' : 1,
-    'b3' : 1,
+    'a3' : 0,
+    'b1' : 0,
+    'b2' : 0,
+    'b3' : 0,
     'c1' : 0,
-    'c2' : 2,
-    'c3' : 1,
+    'c2' : 0,
+    'c3' : 0,
 }
-
-
-
 
 def printkolka (kolka):
         for key , value in kolka.items():
@@ -29,12 +26,39 @@ def printkolka (kolka):
             elif key == "a3" :
                 print()
         
-
+def ruchpla(kloka):
+    wyb = input("gdzie chcesz postawić x:")
+    if wyb == 'a1' :
+        kolka['a1'] = 1
+    elif wyb == 'a2' :
+        kolka['a2'] = 1
+    elif wyb == 'a3' :
+        kolka['a3'] = 1
+    elif wyb == 'b1' :
+        kolka['b1'] = 1
+    elif wyb == 'b2' :
+        kolka['b2'] = 1
+    elif wyb == 'b3' :
+        kolka['b3'] = 1
+    elif wyb == 'c1' :
+        kolka['c1'] = 1
+    elif wyb == 'c2' :
+        kolka['c2'] = 1
+    elif wyb == 'c3' :
+        kolka['c3'] = 1
+    else :
+        print(f"nie ma takiego pola jak {wyb}")
         
 
 
-printkolka(kolka)
-print()
+
+while True :
+    printkolka(kolka)
+    print()
+    ruchpla(kolka)
+
+
+
 
 
 
